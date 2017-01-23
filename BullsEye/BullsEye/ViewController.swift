c4a1f36b61b1dbc501bd31a6f2845d8d7404fe89
Umar Khokhar <ukhokhar@uchicago.edu>
2017-01-22 19:48:22 -0600

    bullseye p66

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -16,8 +16,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,16 +25,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert(){
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" +
+                    "\nThe target value is: \(targetValue)"
+        
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
                                       preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 
 
